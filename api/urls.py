634db35035f879b0b245d5317e2d08cfd5cc3f6e
Rubens <rubensfcontato@gmail.com>
2022-10-26 21:47:09 -0300

    feat: Habilitando rotas

--- api/urls.py
+++ api/urls.py
@@ -13,9 +13,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path
+from django.urls import include, path
+from rest_framework import routers
+from api.views import UserViewSet, PatientViewSet, SchedulingViewSet
+
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet)
+router.register(r'patients', PatientViewSet)
+router.register(r'schedulings', SchedulingViewSet)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('/api', include(router.urls)),
 ]
