d0b1877e9c745e1b84d7c8e78906684b61d6a363
Rubens <rubensfcontato@gmail.com>
2022-10-28 16:08:47 -0300

    fix: Correção na descrição do agendamento

--- app/models.py
+++ app/models.py
@@ -28,14 +28,13 @@ class Patient(models.Model):
 
 #Agendamento
 class Scheduling(models.Model):
-    description = models.TextField(models.TextField(("")))
+    description = models.TextField()
     status = models.CharField(max_length=100)
     medic = models.ForeignKey(User, on_delete=models.CASCADE)
     patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
     createat = models.DateTimeField()
     def __str__(self):
        return self.description
-    
     
 
 
