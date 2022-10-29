dc77ae2dc59442c77419a9d32e994f551eb8de1b
Rubens <rubensfcontato@gmail.com>
2022-10-29 00:48:49 -0300

    fix: Configuração database

--- app/forms.py
+++ app/forms.py
@@ -5,5 +5,5 @@ class UserMaskAdmin(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super(UserMaskAdmin, self).__init__(*args, **kwargs)
         self.fields['phone'].widget.attrs['class'] = 'mask-phone'
-        #self.fields['cep'].widget.attrs['class'] = 'mask-cep'
+        self.fields['cep'].widget.attrs['class'] = 'mask-cep'
 
--- app/settings.py
+++ app/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 import os
 from pathlib import Path
+import dj_database_url
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -78,14 +80,14 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('POSTGRES_NAME'),
-        'USER': os.environ.get('POSTGRES_USER'),
-        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
-        'HOST': os.environ.get('POSTGRES_HOST'),
-        'PORT': os.environ.get('POSTGRES_PORT'),
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
     }
 }
+
+
+
+DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 
 
 # Password validation
